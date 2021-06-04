a7c5224346d731f22da631eb4576670d6ffca885
alexisligon <aligo@macbook-pro.home>
2021-06-04 14:42:56 -0400

    switch statement for different job roles

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const employeeQuestions = [
+const questions = [
     { //new employee, if no then end questions
-        type: 'list',
+        type: 'confirm',
         message: 'New Employee?',
         name: 'new',
-        choices: ['Yes', 'No']
     },
     {
         type: 'input',
@@ -26,7 +25,7 @@ const employeeQuestions = [
     {
         type: 'list',
         message: 'Type of Employee',
-        name: 'type',
+        name: 'role',
         choices: ['Manager', 'Engineer', 'Intern']
     },
 ]
@@ -68,6 +67,35 @@ const anotherQuestion = [
     }
 ]
 
-inquirer.prompt(questions).then(() => {
-    console.log('all questions answered!')
-})
+inquirer.prompt(questions).then((answer) => {
+    //switch statement for other question prompts based on type of job role
+    switch (answer.role) {
+        case 'Manager':
+            inquirer.prompt(managerQuestion).then(()=>{
+                console.log('all questions answered!')
+            });
+            break;
+        case 'Engineer':
+            inquirer.prompt(engineerQuestion).then(()=>{
+                console.log('all questions answered!')
+            });
+            break;
+        case 'Intern':
+            inquirer.prompt(internQuestion).then(()=>{
+                console.log('all questions answered!')
+            });
+            break;
+    }
+    })
+
+//start with employee questions
+//if yes to new employee, continue
+//if no, end application
+
+//if manager selected, start manager questions
+//if engineer selected, start engineer questions
+//if intern selected, start intern questions
+
+//always end with anotherQuestion
+//if yes to add another employee, start app over
+//if no, end application
