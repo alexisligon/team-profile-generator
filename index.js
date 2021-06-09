dff4a42c2d6077e74fee5f2f14c6dbcc91774f38
alexisligon <aligo@macbook-pro.home>
2021-06-08 22:23:52 -0400

    added more styling, needs a few small changes

--- index.js
+++ index.js
@@ -91,17 +91,25 @@ const internQuestion = [
     }
 ];
 
+//function to complete the file
 generateTeam = () => {
     fs.appendFile('./dist/team.html',
         `
-    </body>
-    </html>`,
+        </body>
+        <script
+          src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+          integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"
+          integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT"
+          crossorigin="anonymous"
+        ></script>
+      </html>`,
         (err) => err ? console.log(err) : console.log('Team has been built!'))
 }
 
-writeFile = () => {
-
-}
 //function for adding engineer, intern, or generate team
 addOrGenerate = () => {
     inquirer.prompt([{
@@ -127,26 +135,23 @@ addOrGenerate = () => {
                 employees.push(engineer);
 
                 fs.appendFile('./dist/team.html',
-                `
-                <div>
-                <div>
-                    <h2>${engineer.getName()}</h2>
-                    <h3>${engineer.getRole()}</h3>
-                </div>
-
-                <!--card info section-->
-                <div>
-                    <ul>
-                    <li>ID: ${engineer.getId()}</li>
-                    <!--link to email-->
-                    <li>EMAIL: ${engineer.getEmail()}</li>
-                    <li>Github: ${engineer.getGit()}</li>
+                    `
+                <div class="col-sm-6" style="width: 18rem">
+                 <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">${engineer.getName()}</h5>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item text-center">${engineer.getRole()}</li>
+                        <li class="list-group-item">Email: <a href="mailto: ${engineer.getEmail()}" class="card-link">${engineer.getEmail()}</a></li>
+                        <li class="list-group-item">ID: ${engineer.getId()}</li>
+                        <li class="list-group-item">Github Username:<a href="https://github.com/${engineer.getGit()}">${engineer.getGit()}</a></li>
                     </ul>
+                    </div>
                 </div>
-                </div>
-
-                `, 
-                (err) => err ? console.log(err) : console.log('Successfully added employee!'))
+                
+                `,
+                    (err) => err ? console.log(err) : console.log('Successfully added employee!'))
 
                 if (engineerAnswer.add == true) {
                     addOrGenerate();
@@ -172,25 +177,23 @@ addOrGenerate = () => {
 
                 fs.appendFile('./dist/team.html',
                 `
-                <div>
-                <div>
-                    <h2>${intern.getName()}</h2>
-                    <h3>${intern.getRole()}</h3>
-                </div>
-
-                <!--card info section-->
-                <div>
-                    <ul>
-                    <li>ID: ${intern.getId()}</li>
-                    <!--link to email-->
-                    <li>Email: ${intern.getEmail()}</li>
-                    <li>School: ${intern.getSchool()}</li>
+                <div class="col-sm-6" style="width: 18rem">
+                    <div class="card" >
+                    <div class="card-body">
+                        <h5 class="card-title text-center">${intern.getName()}</h5>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item text-center">${intern.getRole()}</li>
+                        <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}" class="card-link">${intern.getEmail()}</a></li>
+                        <li class="list-group-item">ID: ${intern.getId()}</li>
+                        <li class="list-group-item">School: ${intern.getSchool()}</li>
                     </ul>
+                    </div>
                 </div>
                 </div>
                 
-                `, 
-                (err) => err ? console.log(err) : console.log('Successfully added employee!'))
+                `,
+                    (err) => err ? console.log(err) : console.log('Successfully added employee!'))
                 if (internAnswer.add == true) {
                     addOrGenerate();
                 } else {
@@ -223,37 +226,48 @@ startQuestions = () => {
         employees.push(manager);
 
         fs.writeFile('./dist/team.html',
-            `
+        `
         <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+      crossorigin="anonymous"
+    ></script>
     <title>My Team</title>
   </head>
   <body>
     <h1>My Team</h1>
 
-             <div>
-                <div>
-                    <h2>${manager.getName()}</h2>
-                    <h3>${manager.getRole()}</h3>
-                </div>
+    <div class="row">
 
-                <!--card info section-->
-                <div>
-                    <ul>
-                    <li>ID: ${manager.getId()}</li>
-                    <!--link to email-->
-                    <li>Email: ${manager.getEmail()}</li>
-                    <li>School: ${manager.getOfficeNumber()}</li>
-                    </ul>
-                </div>
-            </div>
+      <div class="col-sm-6">
+        <div class="card" style="width: 18rem">
+          <div class="card-body">
+            <h5 class="card-title text-center">${manager.getName()}</h5>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item text-center"><dt>${manager.getRole()}</dt></li>
+            <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}" class="card-link">${manager.getEmail()}</a></li>
+            <li class="list-group-item">ID: ${manager.getId()}</li>
+            <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
+          </ul>
+        </div>
+      </div>
+        
+        `,
     
-        `, 
-        (err) => err ? console.log(err) : console.log('Successfully added employee!'))
+            (err) => err ? console.log(err) : console.log('Successfully added employee!'))
 
         addOrGenerate();
     });
